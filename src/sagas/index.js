eef61438178075c1a59de180564b455c22b2f671
m.novikov <m.novikov@wastegroup.ru>
2019-12-16 19:28:47 +0300

    add few components

---
+++ src/sagas/index.js
@@ -0,0 +1,19 @@
+// import { takeEvery, put } from 'redux-saga/effects';
+
+// import { IMAGES } from '../constants';
+
+// function* handleImagesLoad() {
+//     console.log('fetching images from unsplash');
+// }
+
+// function* dang() {
+//     console.log('dang');
+// }
+
+// // watcher saga
+// function* rootSaga() {
+//     yield takeEvery(IMAGES.LOAD, handleImagesLoad);
+//     yield takeEvery('DANG', dang);
+// }
+
+// export default rootSaga;
